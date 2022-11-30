0a69cc9632dbe6d5cd0f9707b148e3daa3a5118f
amihaiemil <amihaiemil@gmail.com>
2022-11-30 17:02:27 +0200

    kolasu and entrypoint

---
+++ src/main/java/org/queenlang/transpiler/EntryPoint.java
@@ -0,0 +1,13 @@
+package org.queenlang.transpiler;
+
+import java.util.Arrays;
+
+/**
+ * The transpiler's entrypoint.
+ */
+public final class EntryPoint {
+
+    public static void main(String[] args) {
+        System.out.println("QUEEN OF JAVA ARGS: " + Arrays.toString(args));
+    }
+}

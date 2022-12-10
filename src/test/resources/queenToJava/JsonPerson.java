20738ee6d7201f15d89ae5aa060fde01dbdbe09a
amihaiemil <amihaiemil@gmail.com>
2022-12-10 22:18:28 +0200

    test

--- src/test/resources/queenToJava/JsonPerson.java
+++ src/test/resources/queenToJava/JsonPerson.java
@@ -8,12 +8,20 @@ public final class JsonPerson implements Person {
     @NotNull
     private final JsonObject json;
 
-    public JsonPerson() {
-        this(Json.createObjectBuilder().build());
+    public JsonPerson(final InputStream jsonStream) {
+        super();
+        try {
+            this.json = Json.readStream(jsonStream).toObject();
+        } catch (final IOException ex) {
+            throw new IllegalStateException(ex);
+        }
     }
 
     public JsonPerson(final JsonObject json) {
         super();
         this.json = json;
+        if (this.json == null) {
+            throw new Exception("NOT OK");
+        }
     }
 }

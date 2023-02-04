2499670a0e1093d4917d1cff62a2aa7511252cea
amihaiemil <amihaiemil@gmail.com>
2023-02-04 23:23:54 +0200

    tests

--- src/test/resources/queenToJava/random/Expressions.java
+++ src/test/resources/queenToJava/random/Expressions.java
@@ -14,4 +14,22 @@ public final class Expressions implements ExpressionsTest {
     public Student castingLambda(final Object obj) {
         return (Student) () -> obj;
     }
+
+    @Override
+    public Student returnLambda(final Object obj) {
+        return (firstName, lastName) -> new Student(firstName, lastName);
+    }
+
+    @Override
+    public Student returnLambdaEmptyStudent(final Object obj) {
+        Supplier<Student> supplier = firstName -> new Student(firstName);
+        Supplier<Student> another = (firstName, lastName, grades) -> {
+            final Student s = new Student();
+            s.setFirstName(firstName);
+            s.setLastName(lastName);
+            s.setGrades(grades);
+            return s;
+        };
+        return () -> new Student(firstName, lastName);
+    }
 }

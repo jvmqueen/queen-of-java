3014de4aa85f2aba0f0da1cf40034a2fea7668bd
Mihai A. 🇷🇴🇩🇪🇬🇧🇫🇷 <amihaiemil@gmail.com>
2026-07-20 17:23:42 +0300

    Update rrv.sh

--- rrv.sh
+++ rrv.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 # Rultor release versioning script for Maven projects.
 #
-#
-# It looks for the project’s version, which MUST respect the pattern 
-# [0-9]*\.[0-9]*\.[0-9]*-SNAPSHOT and BE THE FIRST MATCH in pom.xml
-#
 # What it does: updates the pom.xml version of the project according to
 # the variable ${tag} provided to rultor. Specifically, it increments the 
 # 3rd digit and adds '-SNAPSHOT' to it.
-#
+# 
+# Also updates various places in the README file where the latest version should
+# be specified.
 #
 # IMPORTANT:
 #     the given tag has to contain 3 numbers separated by dots!
 #     
-#     e.g. tag = 1.0.1 or tag = 3.2.53 will result in new versions of 1.0.2-SNAPSHOT
-#     or 3.2.54-SNAPSHOT
+#     e.g. tag = 1.0.1 or tag = 3.2.53 will result in new iteration version of 1.0.2-SNAPSHOT
+#     or 3.2.54-SNAPSHOT respectively.
 
 
 [[ "${tag}" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]] || exit -1
